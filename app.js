99f22be1decfb6fecea220099b57e5f761e137f1
Ziyad Hammad <ZHAMMAD@hubgroup.com>
2022-12-25 18:27:46 -0500

    Worked on css file

--- app.js
+++ app.js
@@ -100,7 +100,7 @@ String.prototype.shuffle = function () {
 
 const floating_btn = document.querySelector('.floating-btn');
 const close_btn = document.querySelector('.close-btn');
-const social_panel_container = document.querySelector('.social-panel-container');
+const social_panel_container = document.querySelector('.social-container');
 
 floating_btn.addEventListener('click', () => {
 	social_panel_container.classList.toggle('visible')
